5f45455a70a06ffc38006abdfc8774b6e3e7f228
vhongkol <pradillabong01@gmail.com>
2023-01-06 16:52:02 +0800

    add login and register functionality

--- src/backend/app/Http/Controllers/AuthController.php
+++ src/backend/app/Http/Controllers/AuthController.php
@@ -6,14 +6,54 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use App\Helper\HasApiResponse;
 use Illuminate\Support\Facades\Auth;
-use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {   
     use HasApiResponse;
     
-    public function register(RegisterRequest $request, User $user)
+    public function register(Request $request, User $user)
     {
+        $rules = [
+            'name' => 'required|string|max:50',
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
+            'address' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ];
+        
+        $messages = [
+            'name.required' => 'Name is required field!',
+            'name.string' => 'Name must be string!',
+
+            'first_name.required' => 'First name is required field!',
+            'first_name.string' => 'First name must be string!',
+
+            'last_name.require' => 'Last name is required field!',
+            'last_name.string' => 'Last name must be string!',
+
+            'address' => 'Address is required field!',
+            'address.string' => 'Address must be string!',
+
+            'email.required' => 'Email is required field!',
+            'email.email' => 'Email is Invalid!',
+            'email.unique:users' => 'Email is required field!',
+
+            'password' => 'Password is required!',
+            
+            'confirm_password.required' => 'Password is required!',
+            'confirm_password.same:password' => 'Password and confirm password must match!',
+        ];
+
+        $validation = Validator::make($request->all(), $rules, $messages);
+        if($validation->fails())
+        {
+            return $this->httpBadRequestError($validation->errors(), []);
+        } 
+   
         $user = $user->saveUser($request);
 
         return $this->httpCreated($user, 'User created successfully!');
